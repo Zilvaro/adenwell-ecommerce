afa3660625db9ac8f2311c639f0c7ef3b3ccbdbf
Zilvaro <zilvinas@digitalcase.ch>
2022-10-21 12:50:26 +0000

    added home app and templates

--- aden_store/settings.py
+++ aden_store/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'adenapp',
+
 
 
 ]
@@ -49,7 +51,10 @@ ROOT_URLCONF = 'aden_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ adenapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdenappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'adenapp'
