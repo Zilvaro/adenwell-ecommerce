488e5d860d0274280822afe83f44998ea11dcadc
Zilvaro <zilvinas@digitalcase.ch>
2022-11-10 19:14:12 +0000

    added stripe and basic checkout functionality

--- aden_store/settings.py
+++ aden_store/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'stripe',
 ]
 
 MIDDLEWARE = [
@@ -74,7 +75,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # req by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
