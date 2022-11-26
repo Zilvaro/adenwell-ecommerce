465f94f968ae2604b5931ef0adab81b73404534d
Zilvaro <zilvinas@digitalcase.ch>
2022-11-26 05:54:05 +0000

    sets the main route for index.html

--- adenapp/urls.py
+++ adenapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.index, name='home'),
+    path("home/", views.HomeView.as_view(), name='home_view'),
 ]
